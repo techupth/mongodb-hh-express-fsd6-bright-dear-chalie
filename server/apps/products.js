786b680fb5939371546de2c6ae9926e89f83e836
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-06-28 16:04:54 +0700

    feat: built API for creating a new product

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,15 +1,33 @@
-import { Router } from "express";
+import { Router } from 'express';
+
+import { db } from '../utils/db.js';
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+productRouter.get('/', (req, res) => {});
 
-productRouter.get("/:id", (req, res) => {});
+productRouter.get('/:id', (req, res) => {});
 
-productRouter.post("/", (req, res) => {});
+productRouter.post('/', async (req, res) => {
+  const collection = db.collection('products');
+  const productData = { ...req.body };
+  try {
+    const products = await collection.insertOne(productData);
+    console.log(products);
+  } catch (error) {
+    return res.status(500).json({
+      message:
+        'Server could not create a new question because of database connection'
+    });
+  }
 
-productRouter.put("/:id", (req, res) => {});
+  return res
+    .status(200)
+    .json({ message: 'Product has been created successfully' });
+});
 
-productRouter.delete("/:id", (req, res) => {});
+productRouter.put('/:id', (req, res) => {});
+
+productRouter.delete('/:id', (req, res) => {});
 
 export default productRouter;
